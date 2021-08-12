d06dde4fe31b5e3c20963f03f29ba792e0a1f2b4
mukul <64519940+kavinjeveriya@users.noreply.github.com>
2021-08-12 22:52:58 +0530

    Add files via upload

---
+++ Client.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import cv2, socket, pickle, os
+
+
+# In[2]:
+
+
+s=socket.socket(socket.AF_INET , socket.SOCK_DGRAM)
+serverip="192.168.43.174"
+serverport=307
+
+
+# In[31]:
+
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret,photo = cap.read()
+    cv2.imshow('my pic', photo)
+    ret, buffer = cv2.imencode(".jpg", photo, [int(cv2.IMWRITE_JPEG_QUALITY),30])
+    x_as_bytes = pickle.dumps(buffer)
+    s.sendto(x_as_bytes,(serverip , serverport))
+    if cv2.waitKey(10) == 13:
+        break
+  
+cv2.destroyAllWindows()
+cap.release()
+
+
+# In[ ]:
+
+
+
+
